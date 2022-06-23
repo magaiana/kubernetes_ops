d43f6fb84ff22a9c61e8fd37d5a5d9ed37f9937c
Vusumuzi Magayiyana <VusumuziM@ragingriver.co.za>
2022-06-23 11:03:05 +0200

    changes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+app.UseSwagger();
 
 app.MapGet("/", () => "Hello World!");
 
+app.UseSwaggerUI();
 app.Run();
